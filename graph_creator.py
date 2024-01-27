b4df4b25a73bf5ba75a312a6a32cbbe256b3d12e
Ucutum <t.musikov@yandex.ru>
2024-01-27 10:13:38 +0300

    add stock ai

--- graph_creator.py
+++ graph_creator.py
@@ -2,6 +2,10 @@ import pandas as pd
 import mplfinance as mpf
 from csv_parser import request_stocks
 from datetime import date
+from stock_ai import generate, load_model, grounding, ungrounding_one, grounding_one
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
 
 
 def read_data(filename):
@@ -21,13 +25,42 @@ def read_data(filename):
     
 
 def craete_graph(data, company_token):
-    mpf.plot(data, type='candle', savefig=f"static/graph/{company_token}_graph.png")
+    d = data.index[-6]
+    print("!!!", d)
+    mpf.plot(
+        data, type='candle',
+        savefig=f"static/graph/{company_token}_graph.png",
+        vlines=dict(vlines=d,
+                    linewidths=1, alpha=0.5))
 
 
 def main():
-    for company in ["SBER", "GAZP", "YNDX"]:
-        data = request_stocks(date(2020, 1, 1), date(2024, 1, 26), "GAZP")
-        craete_graph(data[-100:], 'SBER')
+    for company in ["GAZP", "VKCO", "SBER", "YNDX"]:
+        data = request_stocks(date(2020, 1, 1), date(2024, 1, 26), company)
+        # data = read_data("models/YNDX_000101_240101.csv", delimiter=';')
+        x = np.array([np.array(data[["open", "close"]].mean(axis=1))[-100:]])
+        x, mxx = grounding_one(x)
+        model = load_model(f"models/{company}_model.h5")
+        p = model.predict(x)
+        p = ungrounding_one(p, mxx)[0]
+
+        last = data['close'][-1]
+        for i in range(5):
+            last_date = data.index[-1]
+            next_day = last_date + timedelta(days=1)
+            prow = {
+                # 'begin': last_date,
+                'open': last,
+                'high': max(last, p[i]) + 1,
+                'low': min(last, p[i]) - 1,
+                'close': p[i],
+                'value': 0,
+                'quantity': 0,
+                'end': 0
+            }
+            last = p[i]
+            data.loc[next_day] = prow
+        craete_graph(data[-100:], company)
 
 
 if __name__ == '__main__':
