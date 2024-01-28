50806273db8a4f91cae6f42afec1b4c14f9fbb22
Ucutum <t.musikov@yandex.ru>
2024-01-28 15:39:45 +0300

    modified:   csv_parser.py

--- csv_parser.py
+++ csv_parser.py
@@ -1,28 +1,15 @@
 import pandas as pd
 import mplfinance as mpf
 import requests
-from datetime import date
-from moeximporter import MoexImporter, MoexSecurity, MoexCandlePeriods
+import datetime
+import yfinance as yf
 
-mi = MoexImporter()
-
-def request_stocks(date_from: date, date_to: date, symbol: str):
-    sec = MoexSecurity(symbol, mi)
-    return sec.getCandleQuotesAsDataFrame(date_from, date_to, interval=MoexCandlePeriods.Period1Day)
+def request_stocks(start: datetime.datetime, symbol: str):
+    data = yf.download(symbol, period="1d", start=start.strftime("%Y-%m-%d"))
+    return data
 
 def read_data(filename: str) -> pd.DataFrame:
     data = pd.read_csv(filename, delimiter=';')
-    data = data[['<DATE>', '<OPEN>', '<HIGH>', '<LOW>', '<CLOSE>', '<VOL>']]
-    data['<DATE>'] = pd.to_datetime(data['<DATE>'], format='%d/%m/%y')
-    data['<OPEN>'] = pd.to_numeric(data['<OPEN>'])
-    data['<HIGH>'] = pd.to_numeric(data['<HIGH>'])
-    data['<LOW>'] = pd.to_numeric(data['<LOW>'])
-    data['<CLOSE>'] = pd.to_numeric(data['<CLOSE>'])
-    data['<VOL>'] = pd.to_numeric(data['<VOL>'])
-    data.set_index('<DATE>', inplace=True)
-    data.rename(columns={
-        '<OPEN>': 'Open', '<HIGH>': 'High', '<LOW>': 'Low', '<CLOSE>': 'Close',
-        '<VOL>': 'Volume'}, inplace=True)
     return data
 
 def write_data(df: pd.DataFrame, filename):
@@ -30,4 +17,6 @@ def write_data(df: pd.DataFrame, filename):
     
 
 if __name__ == '__main__':
-    write_data(request_stocks(date(2019, 1, 1), date(2024, 1, 26), "GAZP"), "GAZP.csv")
+    # request_stocks(datetime.datetime(2020, 1, 1), "NVDA")
+    write_data(request_stocks(datetime.datetime(2000, 1, 1), "AAPL"), "AAPL.csv")
+    # print(read_data("AAPL.csv"))
