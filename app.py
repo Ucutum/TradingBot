38a60d89454f3bcc5c3e01fc5b0987f766757253
Keopint <sutir2546@mail.ru>
2024-02-03 10:41:18 +0300

    add cancel_subscription

--- app.py
+++ app.py
@@ -216,13 +216,34 @@ def singup_page():
                         return redirect(url_for("login_page"))
     return render_template('singup_page.html', form=form)
 
-
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('cover_page'))
 
+@app.route('/cancel_subscription')
+def cancel_subscription_page():
+    if not current_user.is_authenticated:
+        return abort(403)
+    return render_template('cancel_subscription_page.html')
+
+@app.route('/cover2', methods=['GET', 'POST'])
+def cover2_page():
+    if current_user.is_authenticated:
+        if not current_user.subscription:
+            return redirect(url_for('на покупку'))
+    if request.method == 'POST':
+        if not current_user.is_authenticated:
+            flash("Войдите в систему для покупки")
+            return redirect(url_for('login_page'))
+        else:
+            session = create_session()
+            user = session.query(User).get(current_user.id)
+            user.subscription = False
+            session.commit()
+            return redirect(url_for('cancel_subscription_page'))
+    return render_template('cover2_page.html')
 
 @app.route("/ordering")
 def ordering_page():
